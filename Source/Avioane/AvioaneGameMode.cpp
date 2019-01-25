6f253b8c7282cdebd9ea2ea4f012d1f13ba01b62
Atestat2019 <46197107+Atestat2019@users.noreply.github.com>
2019-01-25 23:27:26 +0200

    Initial commit

---
+++ Source/Avioane/AvioaneGameMode.cpp
@@ -0,0 +1,13 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "AvioaneGameMode.h"
+#include "AvioanePlayerController.h"
+#include "AvioanePawn.h"
+
+AAvioaneGameMode::AAvioaneGameMode()
+{
+	// no pawn by default
+	DefaultPawnClass = AAvioanePawn::StaticClass();
+	// use our own player controller class
+	PlayerControllerClass = AAvioanePlayerController::StaticClass();
+}
---
+++ Source/Avioane/AvioanePawn.cpp
@@ -0,0 +1,100 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "AvioanePawn.h"
+#include "AvioaneBlock.h"
+#include "HeadMountedDisplayFunctionLibrary.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "Engine/World.h"
+#include "DrawDebugHelpers.h"
+
+AAvioanePawn::AAvioanePawn(const FObjectInitializer& ObjectInitializer) 
+	: Super(ObjectInitializer)
+{
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+void AAvioanePawn::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UHeadMountedDisplayFunctionLibrary::IsHeadMountedDisplayEnabled())
+		{
+			if (UCameraComponent* OurCamera = PC->GetViewTarget()->FindComponentByClass<UCameraComponent>())
+			{
+				FVector Start = OurCamera->GetComponentLocation();
+				FVector End = Start + (OurCamera->GetComponentRotation().Vector() * 8000.0f);
+				TraceForBlock(Start, End, true);
+			}
+		}
+		else
+		{
+			FVector Start, Dir, End;
+			PC->DeprojectMousePositionToWorld(Start, Dir);
+			End = Start + (Dir * 8000.0f);
+			TraceForBlock(Start, End, false);
+		}
+	}
+}
+
+void AAvioanePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("OnResetVR", EInputEvent::IE_Pressed, this, &AAvioanePawn::OnResetVR);
+	PlayerInputComponent->BindAction("TriggerClick", EInputEvent::IE_Pressed, this, &AAvioanePawn::TriggerClick);
+}
+
+void AAvioanePawn::CalcCamera(float DeltaTime, struct FMinimalViewInfo& OutResult)
+{
+	Super::CalcCamera(DeltaTime, OutResult);
+
+	OutResult.Rotation = FRotator(-90.0f, -90.0f, 0.0f);
+}
+
+void AAvioanePawn::OnResetVR()
+{
+	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+}
+
+void AAvioanePawn::TriggerClick()
+{
+	if (CurrentBlockFocus)
+	{
+		CurrentBlockFocus->HandleClicked();
+	}
+}
+
+void AAvioanePawn::TraceForBlock(const FVector& Start, const FVector& End, bool bDrawDebugHelpers)
+{
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, Start, End, ECC_Visibility);
+	if (bDrawDebugHelpers)
+	{
+		DrawDebugLine(GetWorld(), Start, HitResult.Location, FColor::Red);
+		DrawDebugSolidBox(GetWorld(), HitResult.Location, FVector(20.0f), FColor::Red);
+	}
+	if (HitResult.Actor.IsValid())
+	{
+		AAvioaneBlock* HitBlock = Cast<AAvioaneBlock>(HitResult.Actor.Get());
+		if (CurrentBlockFocus != HitBlock)
+		{
+			if (CurrentBlockFocus)
+			{
+				CurrentBlockFocus->Highlight(false);
+			}
+			if (HitBlock)
+			{
+				HitBlock->Highlight(true);
+			}
+			CurrentBlockFocus = HitBlock;
+		}
+	}
+	else if (CurrentBlockFocus)
+	{
+		CurrentBlockFocus->Highlight(false);
+		CurrentBlockFocus = nullptr;
+	}
+}
